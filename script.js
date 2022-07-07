4e0f590124b6b2a89811037214e89f48a8256a74
h <hchenwu02@gmail.com>
2022-07-07 18:14:01 -0400

    Add function to change time based on client's OS

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
-let metric = document.querySelector(".metric");
-let imperial = document.querySelector(".imperial");
+//Change the time display to be the same as the one from the client's OS when the website loads
+const date = new Date();
+const dateObject = document.querySelector(".date");
+const timeObject = document.querySelector(".current-time");
+let day;
+
+switch (date.getDay()) {
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 2:
+        day = "Saturday";
+            break;
+}
+
+dateObject.textContent = day + ", " + (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear();
+timeObject.textContent = date.getHours() + ":" + String(date.getMinutes()).padStart(2, '0');
+
+
+
+
+const metric = document.querySelector(".metric");
+const imperial = document.querySelector(".imperial");
 
 metric.addEventListener("click", () => {
     console.log("Hello")
@@ -9,3 +45,4 @@ imperial.addEventListener("click", () => {
     console.log("Bye")
 });
 
+console.log(date);
