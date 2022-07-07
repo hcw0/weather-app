0d4ace967d552cd004e1fff9d3c6d5093af504a5
h <hchenwu02@gmail.com>
2022-07-07 01:33:03 -0400

    Fix bug where gif doesn't resize

---
+++ script.js
@@ -0,0 +1,11 @@
+let metric = document.querySelector(".metric");
+let imperial = document.querySelector(".imperial");
+
+metric.addEventListener("click", () => {
+    console.log("Hello")
+});
+
+imperial.addEventListener("click", () => {
+    console.log("Bye")
+});
+
